529e980496732adceef4fc51df8797e6bf1e638e
APPLE <apple@APPLEdeMacBook-Pro-2.local>
2021-09-23 21:05:16 +0100

    Added my birthday to hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-name = 'Josephine'
-age = 36
-
-print(f'Hi, my name is {name}, and I am {age} years old.')
+name = 'Xinyue Qin'
+age = 22
+birthday='24/02/1999'
+print(f'Hi, my name is {name}, my birthday is {birthday}, and I am {age} years old.')
